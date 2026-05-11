45f87cd84c29642fcdb2476ee705b9f0064069d9
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-05-11 12:56:12 +0200

    Create dovecot_impersonate.php

---
+++ roundcube-plugins/dovecot_impersonate/dovecot_impersonate.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+  * This plugin lets you impersonate another user using a master login. Only works with dovecot.
+  * 
+  * http://wiki.dovecot.org/Authentication/MasterUsers
+  * 
+  * @author Cor Bosman (roundcube@wa.ter.net)
+  */
+  
+class dovecot_impersonate extends rcube_plugin {
+  
+  public function init() 
+  {    
+    $this->add_hook('storage_connect', array($this, 'impersonate'));
+    $this->add_hook('managesieve_connect', array($this, 'impersonate'));
+    $this->add_hook('authenticate', array($this, 'login'));  
+    $this->add_hook('sieverules_connect', array($this, 'impersonate_sieve'));  
+  }
+  
+  function login($data) {
+    // find the seperator character
+    $rcmail = rcmail::get_instance();
+    $this->load_config();
+    
+    $seperator = $rcmail->config->get('dovecot_impersonate_seperator', '*');
+    
+    if(strpos($data['user'], $seperator)) {
+      $arr = explode($seperator, $data['user']);
+      if(count($arr) == 2) {
+        $data['user'] = $arr[0];
+        $_SESSION['plugin.dovecot_impersonate_master'] = $seperator . $arr[1];
+      }
+    }
+    return($data);
+  }
+  
+  function impersonate($data) {
+    if(isset($_SESSION['plugin.dovecot_impersonate_master'])) {
+      $data['user'] = $data['user'] . $_SESSION['plugin.dovecot_impersonate_master']; 
+    }
+    return($data);
+  }
+  
+  function impersonate_sieve($data) {
+    if(isset($_SESSION['plugin.dovecot_impersonate_master'])) {
+      $data['username'] = $data['username'] . $_SESSION['plugin.dovecot_impersonate_master']; 
+    }
+    return($data);
+  }
+  
+}
+?>
